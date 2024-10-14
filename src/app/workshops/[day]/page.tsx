5f279a87efbcdc42c02228825430e233bbc7a7a2
raihahahan <mraihandev@gmail.com>
2024-10-14 17:28:55 +0800

    Make workshops page dynamic

---
+++ src/app/workshops/[day]/page.tsx
@@ -0,0 +1,38 @@
+import { workshopsMockData } from "@/components/workshops/data";
+import RegisterButton from "@/components/workshops/RegisterButton";
+import { IWorkShop } from "@/components/workshops/types";
+import WorkshopList from "@/components/workshops/WorkshopList";
+
+export default async function Page({ params }: { params: { day: string } }) {
+  const day = params.day;
+  const workshops = await getWorkshopsAsync(day);
+
+  return (
+    <div className="bg-blue-900 min-h-screen p-8">
+      <h1 className="text-2xl text-center text-white font-bold mb-8">
+        <div className="[text-shadow:_4px_3px_0px_#000000] text-5xl">
+          WORKSHOPS
+        </div>
+        {getDayString(day)}
+      </h1>
+      <WorkshopList workshops={workshops} />
+      <div className="flex justify-center">
+        <RegisterButton />
+      </div>
+    </div>
+  );
+}
+
+function getDayString(day: string): string {
+  // Check if the string starts with 'day' and the rest is a number
+  if (day.startsWith("day") && !isNaN(+day.slice(3))) {
+    return `DAY ${day.slice(3)}`;
+  }
+  return day;
+}
+
+async function getWorkshopsAsync(day: string): Promise<IWorkShop[]> {
+  const workshops: IWorkShop[] | null = workshopsMockData[day];
+  if (workshops == null) return [];
+  return workshops;
+}
--- src/components/workshops/WorkshopList.tsx
+++ src/components/workshops/WorkshopList.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import WorkshopItem from './WorkshopItem';
+import React from "react";
+import WorkshopItem from "./WorkshopItem";
+import { IWorkShop } from "./types";
 
 interface WorkshopListProps {
-  workshops: { date: string; time: string; imageAlt?: string }[];
+  workshops: IWorkShop[];
 }
 
 const WorkshopList: React.FC<WorkshopListProps> = ({ workshops }) => {
   return (
     <div className="mb-8">
       {workshops.map((workshop, index) => (
-        <WorkshopItem key={index} date={workshop.date} time={workshop.time} imageAlt={workshop.imageAlt} />
+        <WorkshopItem
+          key={index}
+          date={workshop.date}
+          time={workshop.time}
+          imageAlt={workshop.imageAlt}
+          imageSrc={workshop.imageSrc}
+          description={workshop.description}
+        />
       ))}
     </div>
   );
