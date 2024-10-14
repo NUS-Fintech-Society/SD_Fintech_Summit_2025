0d216b80a250f0c41d2554e92044857d5af77377
saladbowl <ron52086@gmail.com>
2024-10-14 23:34:03 +0800

    Code cleanup for ProblemStatement component

--- src/components/home/ProblemStatement.tsx
+++ src/components/home/ProblemStatement.tsx
@@ -1,16 +1,3 @@
-// import React from 'react';
-
-// const ProblemStatement: React.FC = () => {
-//   return (
-//     <div className="mb-8">
-//       <h2 className="text-xl font-semibold text-yellow-300 mb-2">Problem Statement:</h2>
-//       <div className="p-4 bg-white rounded-lg shadow-md text-black">[This is the problem Statement]</div>
-//     </div>
-//   );
-// };
-
-// export default ProblemStatement;
-
 import React from 'react';
 
 const ProblemStatement = () => {
