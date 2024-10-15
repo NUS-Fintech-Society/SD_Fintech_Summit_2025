6dbb6a29bd840f96bf6c84d4f8c56fbb9f00427a
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-10-15 14:12:48 +0800

    add: mobile responsiveness

--- src/components/home/Timeline.tsx
+++ src/components/home/Timeline.tsx
@@ -74,10 +74,13 @@ const Timeline = () => {
               <div className="flex items-center justify-center w-8 h-8 rounded-full bg-[#D9D9D9] shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2 shadow-[0_4px_14px_rgba(0,0,0,0.6)]"></div>
 
               <div className={`relative w-[calc(100%-6rem)] md:w-[calc(50%-4rem)] bg-[#D9D9D9] p-6 rounded-lg border border-[#D9D9D9] shadow-[0_4px_10px_rgba(0,0,0,0.6)] 
-                ${index % 2 === 0 ? 'ml-8' : 'mr-8'} 
+                ${index % 2 === 0 ? 'ml-8' : 'md:mr-8'} 
                 before:absolute before:content-[''] before:w-0 before:h-0 before:border-[32px] before:border-transparent 
-                ${index % 2 === 0 ? 'before:right-full before:border-r-[#D9D9D9]' : 'before:left-full before:border-l-[#D9D9D9]'} 
+                ${index % 2 === 0 
+                  ? 'before:right-full before:border-r-[#D9D9D9]' 
+                  : 'before:right-full before:border-r-[#D9D9D9] md:before:left-full md:before:border-l-[#D9D9D9] md:before:border-r-0 md:before:right-0'}  
                 before:top-1/2 before:-translate-y-1/2`}>
+
                 <div className="text-3xl font-bold text-[#102B71] mb-2">{item.date}</div>
                 <div className="font-bold text-slate-900 mb-2">{item.title}</div>
                 <div className="text-slate-500 mb-4">{item.description}</div>
