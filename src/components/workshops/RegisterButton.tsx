afef09e0c45315dac1a3cf35bc7b5aa62035ffd3
raihahahan <mraihandev@gmail.com>
2024-10-22 05:59:44 +0800

    SCRUM-23: Change register button colour

--- src/components/workshops/RegisterButton.tsx
+++ src/components/workshops/RegisterButton.tsx
@@ -1,10 +1,20 @@
+"use client";
 import React from "react";
 
 const RegisterButton: React.FC = () => {
   return (
     <button
-      style={{ width: 200, height: 60 }}
-      className="bg-blue-500 text-white font-semibold w-358 py-2 px-4 rounded-full hover:bg-blue-600"
+      style={{
+        width: 200,
+        height: 60,
+        backgroundColor: "#4470ba",
+        color: "white",
+        fontWeight: "600",
+        borderRadius: "9999px",
+        padding: "8px 16px",
+      }}
+      onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = "#365a96")}
+      onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = "#4470ba")}
     >
       REGISTER
     </button>
