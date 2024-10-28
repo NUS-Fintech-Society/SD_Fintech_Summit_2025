8800aeb97e896c0febf57e28e0f84e81183ea128
saladbowl <ron52086@gmail.com>
2024-10-29 01:51:59 +0800

    Modify FAQItem to become a dropdown item

--- src/components/faq/FAQItem.tsx
+++ src/components/faq/FAQItem.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+"use client";
+import React, { useState } from "react";
 
 interface FAQItemProps {
   question: string;
@@ -6,10 +7,67 @@ interface FAQItemProps {
 }
 
 const FAQItem: React.FC<FAQItemProps> = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleDropdown = () => {
+    setIsOpen((isOpen) => !isOpen);
+  };
+
   return (
-    <div className="bg-white p-4 rounded-lg shadow-md mb-4">
-      <h3 className="text-lg font-semibold text-gray-800">{question}</h3>
-      <p className="text-gray-600">{answer}</p>
+    <div className={"p-4 rounded-lg shadow-md mb-4 bg-[#dbe6fd]"}>
+      <div
+        className="flex justify-between items-center cursor-pointer"
+        onClick={toggleDropdown}
+      >
+        <div className="flex items-center">
+          <span className="text-xl font-bold text-gray-500 mr-4">01</span>
+          <h3
+            className={`text-lg font-semibold ${
+              isOpen ? "text-black" : "text-gray-800"
+            }`}
+          >
+            {question}
+          </h3>
+        </div>
+        <span
+          className={`w-8 h-8 flex items-center justify-center rounded-full ${
+            isOpen ? "bg-black" : "bg-white"
+          } shadow`}
+        >
+          {isOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-black"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          )}
+        </span>
+      </div>
+      {isOpen && <p className="text-gray-600 mt-4 italic">{answer}</p>}
     </div>
   );
 };
