b624ef76ed958484d1aeaa09e48f8c40cf9b9682
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-11-02 16:23:15 +0800

    Fix: Image responsiveness

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -21,33 +21,25 @@ export default function Page() {
               justifyContent: "space-between",
             }}
           >
-            <h1 className="text-7xl md:text-7xl lg:text-9xl  text-black font-bold mb-4 md:mb-6 lg:mb-8 text-left">
+            <h1 className="text-7xl md:text-7xl lg:text-9xl  text-black font-bold text-left">
               <div className="[text-shadow:_2px_3px_4px_#AAAAAA]">FAQs</div>
             </h1>
-            <div className="lg:hidden flex justify-center md:mb-8">
-              <Image
-                src="/HomeLogoFull.png"
-                alt="Fintech Summit Logo"
-                width={500}
-                height={500}
-              />
-            </div>
-            <h2
-              className="text-5xl md:text-5xl lg:text-7xl mb-4 md:mb-6 lg:mb-8 font-bold"
+          </div>
+        </div>
+        <div className="flex justify-center lg:justify-end my-[-6rem] lg:my-[-10rem]">
+          <Image
+            src="/HomeLogoFull.png"
+            alt="Fintech Summit Logo"
+            width={700}
+            height={700}
+          />
+        </div>
+        <h2
+              className="text-5xl md:text-5xl lg:text-7xl my-4 md:mb-6 lg:mb-8 mt-8 font-bold"
               style={{ color: "#6c93d6" }}
             >
               Opening Ceremony
             </h2>
-          </div>
-          <div className="hidden lg:block mb-8">
-            <Image
-              src="/HomeLogoFull.png"
-              alt="Fintech Summit Logo"
-              width={500}
-              height={500}
-            />
-          </div>
-        </div>
         <FaqAccordion items={mockData.openingCeremony} />
         <h2
           className="text-5xl md:text-5xl lg:text-7xl my-4 md:mb-6 lg:mb-8 mt-8 font-bold"
