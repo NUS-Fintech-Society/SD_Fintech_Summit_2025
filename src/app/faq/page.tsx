b784410faa051b4a5f05046e701d02948cec3665
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-11-02 16:05:33 +0800

    Fix: FAQ responsiveness

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function Page() {
   return (
     <>
-      <div className="bg-white p-24 min-h-screen">
+      <div className="bg-white p-6 md:p-12 lg:p-24 min-h-screen">
         <div
           style={{
             display: "flex",
@@ -21,41 +21,50 @@ export default function Page() {
               justifyContent: "space-between",
             }}
           >
-            <h1 className="text-9xl text-black font-bold mb-8">
+            <h1 className="text-7xl md:text-7xl lg:text-9xl  text-black font-bold mb-4 md:mb-6 lg:mb-8 text-left">
               <div className="[text-shadow:_2px_3px_4px_#AAAAAA]">FAQs</div>
             </h1>
+            <div className="lg:hidden flex justify-center md:mb-8">
+              <Image
+                src="/HomeLogoFull.png"
+                alt="Fintech Summit Logo"
+                width={500}
+                height={500}
+              />
+            </div>
             <h2
-              className="text-7xl mb-8 font-bold"
+              className="text-5xl md:text-5xl lg:text-7xl mb-4 md:mb-6 lg:mb-8 font-bold"
               style={{ color: "#6c93d6" }}
             >
               Opening Ceremony
             </h2>
           </div>
-          <Image
-            src="/HomeLogoFull.png"
-            alt="Fintech Summit Logo"
-            width={500}
-            height={500}
-            className="mb-8"
-          />
+          <div className="hidden lg:block mb-8">
+            <Image
+              src="/HomeLogoFull.png"
+              alt="Fintech Summit Logo"
+              width={500}
+              height={500}
+            />
+          </div>
         </div>
         <FaqAccordion items={mockData.openingCeremony} />
         <h2
-          className="text-7xl mb-8 mt-8 font-bold"
+          className="text-5xl md:text-5xl lg:text-7xl my-4 md:mb-6 lg:mb-8 mt-8 font-bold"
           style={{ color: "#6c93d6" }}
         >
           Hackathon
         </h2>
         <FaqAccordion items={mockData.hackathon} />
         <h2
-          className="text-7xl mb-8 mt-8 font-bold"
+          className="text-5xl md:text-5xl lg:text-7xl mb-4 md:mb-6 lg:mb-8 mt-8 font-bold"
           style={{ color: "#6c93d6" }}
         >
           Workshops
         </h2>
         <FaqAccordion items={mockData.workshops} />
         <h2
-          className="text-7xl mb-8  mt-8 font-bold"
+          className="text-5xl md:text-5xl lg:text-7xl mb-4 md:mb-6 lg:mb-8 mt-8 font-bold"
           style={{ color: "#6c93d6" }}
         >
           Demo day
